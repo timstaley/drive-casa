3cc514d14138a562dd01ccc34d80f576add7c39e
Tim Staley <tim.staley@soton.ac.uk>
2013-05-07 14:44:09 +0100

    Add setup.py, make front-end script virtualenv friendly.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+#with open('requirements.txt') as f:
+#    required = f.read().splitlines()
+
+setup(
+    name="drivecasa",
+    version="0.1.0",
+    packages=['drivecasa'],
+    description="""Some useful CASA scripts, packaged to be callable from elsewhere.""",
+    author="Tim Staley",
+    author_email="timstaley337@gmail.com",
+    url="https://github.com/timstaley/drivecasa",
+#    install_requires=required,
+)
