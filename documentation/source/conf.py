9cafb7a98739dcf3a1d2cd018f11bf4643f6b50d
Tim Staley <git@timstaley.co.uk>
2016-05-27 13:22:46 +0100

    Strip git-sha from docs-version

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -59,7 +59,7 @@ copyright = u'2013, Tim Staley'
 # built documents.
 #
 # The short X.Y version.
-version = drivecasa.__version__
+version = drivecasa.__version__.split('+')[0]
 # The full version, including alpha/beta/rc tags.
 release = version
 
